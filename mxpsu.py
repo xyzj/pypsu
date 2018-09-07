dc43e5b66937012145a2ce413347e6d4db6f2d94
Xu Yuan <nicai@unknow.com>
2018-09-07 16:13:28 +0800

    add PinYin

--- mxpsu.py
+++ mxpsu.py
@@ -22,6 +22,7 @@ import zlib as _xlib
 import sys as _sys
 import json as _json
 import binascii
+from worddata import word_data
 reload(_sys)
 _sys.setdefaultencoding(_sys.getfilesystemencoding())
 
@@ -86,6 +87,38 @@ def int2bcd(value):
     int转bcd
     """
     return ((value / 10) << 4) | (value % 10)
+
+
+class PinYin(object):
+    def __init__(self):
+        self.word_dict = word_data
+
+    def hanzi2pinyin_list(self, string="", firstcode=False):
+        result = []
+        if not isinstance(string, unicode):
+            string = string.decode("utf-8")
+
+        for char in string:
+            key = '%X' % ord(char)
+            value = self.word_dict.get(key, char)
+            outpinyin = str(value).split()[0][:-1].lower()
+            if not outpinyin:
+                outpinyin = char
+            if firstcode:
+                result.append(outpinyin[0])
+            else:
+                result.append(outpinyin)
+
+        return result
+
+    def hanzi2pinyin(self, string="", split="", firstcode=False):
+        """提取中文的拼音
+        @param string:要提取的中文
+        @param split:分隔符
+        @param firstcode: 提取的是全拼还是首字母？如果为true表示提取首字母，默认为False提取全拼
+        """
+        result = self.hanzi2pinyin_list(string=string, firstcode=firstcode)
+        return split.join(result)
 
 
 class GpsPoint():
